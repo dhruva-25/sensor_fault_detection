406cb1cff01f52b6de5781e8dca7573f7dea11a3
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-11-15 23:37:11 +0000

    model trainer

--- sensor/ml/model/estimator.py
+++ sensor/ml/model/estimator.py
@@ -1,5 +1,5 @@
-import os
-
+import os, sys 
+from sensor.exception import SensorException
 class TargetValueMapping:
     def __init__(self):
         self.neg: int = 0
@@ -12,4 +12,21 @@ class TargetValueMapping:
         mapping_response = self.to_dict()
         return dict(zip(mapping_response.values(), mapping_response.keys()))
     
+#Write a code to train model and check the accuracy.
+
+class SensorModel:
     
+    def __init__(self, preprocessor, model):
+        try:
+            self.preprocessor = preprocessor
+            self.model = model
+        except Exception as e:
+            raise SensorException(e, sys)
+        
+    def predict(self, x):
+        try:
+            x_transform = self.preprocessor.transform(x)
+            y_hat = self.model.predict(x_transform)
+            return y_hat
+        except Exception as e:
+            raise SensorException(e, sys)
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,13 +1,15 @@
 from sensor.entity.config_entity import (TrainingPipelineConfig,DataIngestionConfig,
-                                         DataValidationConfig, DataTransformationConfig)
+                                         DataValidationConfig, DataTransformationConfig, 
+                                         ModelTrainerConfig)
 from sensor.entity.artifact_entity import (DataIngestionArtifact, DataValidationArtifact,
-                                           DataTransformationArtifact)
+                                           DataTransformationArtifact, ModelTrainerArtifact)
 from sensor.exception import SensorException
 import sys,os
 from sensor.logger import logging
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
 from sensor.components.data_transformation import DataTransformation
+from sensor.components.model_trainer import ModelTrainer
 class TrainPipeline:
     is_pipeline_running=False
     def __init__(self):
@@ -46,7 +48,14 @@ class TrainPipeline:
             return data_transformation_artifact
         except Exception as e:
             raise SensorException(e, sys)
-        
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact):
+        try:
+            model_trainer_config = ModelTrainerConfig(training_pipeline_config=self.training_pipeline_config)
+            model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+        except Exception as e:
+            raise SensorException(e, sys)
     def run_pipeline(self):
         try:
     
@@ -55,6 +64,7 @@ class TrainPipeline:
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
             data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
         except  Exception as e:
             TrainPipeline.is_pipeline_running=False
             raise  SensorException(e,sys)
